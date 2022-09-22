496256b4dd5e9ee75b9cc8676aa8828215c6c478
BrendaManrique <brenda@users.github.com>
2022-09-22 08:03:24 -0400

    Adding hardhat network config

--- nft-marketplace-project/hardhat.config.ts
+++ nft-marketplace-project/hardhat.config.ts
@@ -1,8 +1,38 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("@nomiclabs/hardhat-waffle");
+const projectId = 'add your project id here'
+const fs = require('fs')
+const keyData = fs.readFileSync('./p-key.txt', {
+  encoding:'utf8', flag:'r'
+})
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  defaultNetwork: 'hardhat',
+  networks:{
+    hardhat:{
+      chainId: 1337 // config standard 
+    },
+    mumbai:{
+      url:`https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts:[keyData]
+    },
+    mainnet: {
+      url:`https://mainnet.infura.io/v3/${projectId}`,
+      accounts:[keyData]
+    }
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
 
 export default config;
+
